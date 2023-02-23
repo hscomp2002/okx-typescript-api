165ec3a49f638d4c096b9d3e64ac0104ddd97ef7
Hscomp <hscomp2002@gmail.com>
2023-02-23 10:46:42 +0330

    getAlgoOrderHistory

--- src/dto/order.dto.ts
+++ src/dto/order.dto.ts
@@ -1,230 +1,288 @@
 export class OrderInpoutDto {
-    instId!: string;
-    tdMode!: "cash" | "isolated" | "cross";
-    ccy?: string;
-    clOrdId?: string;
-    tag?: string;
-    side!: "buy" | "sell";
-    posSide?: "long" | "short";
-    ordType!:
-        | "market"
-        | "limit"
-        | "post_only"
-        | "fok"
-        | "ioc"
-        | "optimal_limit_ioc";
-    px?: string; //Order price. Only applicable to limit,post_only,fok,ioc order.
-    sz!: string;
-    reduceOnly?: boolean;
-    tgtCcy?: string;
-    banAmend?: boolean;
+  instId!: string;
+  tdMode!: "cash" | "isolated" | "cross";
+  ccy?: string;
+  clOrdId?: string;
+  tag?: string;
+  side!: "buy" | "sell";
+  posSide?: "long" | "short";
+  ordType!:
+    | "market"
+    | "limit"
+    | "post_only"
+    | "fok"
+    | "ioc"
+    | "optimal_limit_ioc";
+  px?: string; //Order price. Only applicable to limit,post_only,fok,ioc order.
+  sz!: string;
+  reduceOnly?: boolean;
+  tgtCcy?: string;
+  banAmend?: boolean;
 }
 
 export class OrderResponseDto {
-    clOrdId!: string;
-    ordId!: string;
-    tag!: string;
-    sCode!: string;
-    sMsg!: string;
+  clOrdId!: string;
+  ordId!: string;
+  tag!: string;
+  sCode!: string;
+  sMsg!: string;
 }
 
 export class OrderDetails {
-    instType!: "SPOT" | "MARGIN" | "SWAP" | "FUTURES" | "OPTION";
-    instId!: string;
-    ccy!: string;
-    ordId!: string;
-    clOrdId!: string;
-    tag!: string;
-    px!: string;
-    sz!: string;
-    pnl!: string;
-    ordType!:
-        | "market"
-        | "limit"
-        | "post_only"
-        | "fok"
-        | "ioc"
-        | "optimal_limit_ioc";
-    side!: string;
-    posSide!: string;
-    tdMode!: string;
-    accFillSz!: string;
-    fillPx!: string;
-    tradeId!: string;
-    fillSz!: string;
-    fillTime!: string;
-    state!: "canceled" | "live" | "partially_filled" | "filled";
-    avgPx!: string;
-    lever!: string;
-    tpTriggerPx!: string;
-    tpTriggerPxType!: string;
-    tpOrdPx!: string;
-    slTriggerPx!: string;
-    slTriggerPxType!: string;
-    slOrdPx!: string;
-    feeCcy!: string;
-    fee!: string;
-    rebateCcy!: string;
-    rebate!: string;
-    tgtCcy!: string;
-    category!:
-        | "normal"
-        | "twap"
-        | "adl"
-        | "full_liquidation"
-        | "partial_liquidation"
-        | "delivery"
-        | "ddh";
-    uTime!: string;
-    cTime!: string;
+  instType!: "SPOT" | "MARGIN" | "SWAP" | "FUTURES" | "OPTION";
+  instId!: string;
+  ccy!: string;
+  ordId!: string;
+  clOrdId!: string;
+  tag!: string;
+  px!: string;
+  sz!: string;
+  pnl!: string;
+  ordType!:
+    | "market"
+    | "limit"
+    | "post_only"
+    | "fok"
+    | "ioc"
+    | "optimal_limit_ioc";
+  side!: string;
+  posSide!: string;
+  tdMode!: string;
+  accFillSz!: string;
+  fillPx!: string;
+  tradeId!: string;
+  fillSz!: string;
+  fillTime!: string;
+  state!: "canceled" | "live" | "partially_filled" | "filled";
+  avgPx!: string;
+  lever!: string;
+  tpTriggerPx!: string;
+  tpTriggerPxType!: string;
+  tpOrdPx!: string;
+  slTriggerPx!: string;
+  slTriggerPxType!: string;
+  slOrdPx!: string;
+  feeCcy!: string;
+  fee!: string;
+  rebateCcy!: string;
+  rebate!: string;
+  tgtCcy!: string;
+  category!:
+    | "normal"
+    | "twap"
+    | "adl"
+    | "full_liquidation"
+    | "partial_liquidation"
+    | "delivery"
+    | "ddh";
+  uTime!: string;
+  cTime!: string;
 }
 
 export class OrderListInput {
-    instType?: "SPOT" | "MARGIN" | "SWAP" | "FUTURES" | "OPTION";
-    uly?: string;
-    instId?: string;
-    ordType?:
-        | "market"
-        | "limit"
-        | "post_only"
-        | "fok"
-        | "ioc"
-        | "optimal_limit_ioc";
-    state?: "live" | "partially_filled";
-    after?: string;
-    before?: string;
-    limit?: string = "100";
+  instType?: "SPOT" | "MARGIN" | "SWAP" | "FUTURES" | "OPTION";
+  uly?: string;
+  instId?: string;
+  ordType?:
+    | "market"
+    | "limit"
+    | "post_only"
+    | "fok"
+    | "ioc"
+    | "optimal_limit_ioc";
+  state?: "live" | "partially_filled";
+  after?: string;
+  before?: string;
+  limit?: string = "100";
 }
 
 export class AlgoOrderListInput {
-    ordType!:
-        | "conditional"
-        | "oco"
-        | "trigger"
-        | "move_order_stop"
-        | "iceberg"
-        | "twap";
-    algoId?: string;
-    clOrdId?:string;
-    instType?: "SPOT" | "MARGIN" | "SWAP" | "FUTURES";
-    instId?: string;
-    after?: string;
-    before?: string;
-    limit?: string = "100";
+  ordType!:
+    | "conditional"
+    | "oco"
+    | "trigger"
+    | "move_order_stop"
+    | "iceberg"
+    | "twap";
+  algoId?: string;
+  clOrdId?: string;
+  instType?: "SPOT" | "MARGIN" | "SWAP" | "FUTURES";
+  instId?: string;
+  after?: string;
+  before?: string;
+  limit?: string = "100";
 }
 
 export class AlgoOrderDetails {
-    activePx!: string;
-    actualPx!: string;
-    actualSide!: string;
-    actualSz!: string;
-    algoId!: string;
-    cTime!: string;
-    callbackRatio!: string;
-    callbackSpread!: string;
-    ccy!: string;
-    clOrdId!: string;
-    instId!: string;
-    instType!: string;
-    lever!: string;
-    moveTriggerPx!: string;
-    ordId!: string;
-    ordPx!: string;
-    ordType!: string;
-    posSide!: string;
-    pxLimit!: string;
-    pxSpread!: string;
-    pxVar!: string;
-    side!: string;
-    slOrdPx!: string;
-    slTriggerPx!: string;
-    slTriggerPxType!: string;
-    state!: string;
-    sz!: string;
-    szLimit!: string;
-    tag!: string;
-    tdMode!: string;
-    tgtCcy!: string;
-    timeInterval!: string;
-    tpOrdPx!: string;
-    tpTriggerPx!: string;
-    tpTriggerPxType!: string;
-    triggerPx!: string;
-    triggerPxType!: string;
-    triggerTime!: string;
+  activePx!: string;
+  actualPx!: string;
+  actualSide!: string;
+  actualSz!: string;
+  algoId!: string;
+  cTime!: string;
+  callbackRatio!: string;
+  callbackSpread!: string;
+  ccy!: string;
+  clOrdId!: string;
+  instId!: string;
+  instType!: string;
+  lever!: string;
+  moveTriggerPx!: string;
+  ordId!: string;
+  ordPx!: string;
+  ordType!: string;
+  posSide!: string;
+  pxLimit!: string;
+  pxSpread!: string;
+  pxVar!: string;
+  side!: string;
+  slOrdPx!: string;
+  slTriggerPx!: string;
+  slTriggerPxType!: string;
+  state!: string;
+  sz!: string;
+  szLimit!: string;
+  tag!: string;
+  tdMode!: string;
+  tgtCcy!: string;
+  timeInterval!: string;
+  tpOrdPx!: string;
+  tpTriggerPx!: string;
+  tpTriggerPxType!: string;
+  triggerPx!: string;
+  triggerPxType!: string;
+  triggerTime!: string;
 }
 
-
 export class AlgoOrderInpoutDto {
-    instId!: string;
-    tdMode!: "cash" | "isolated" | "cross";
-    ccy?: string;
-    side!: "buy" | "sell";
-    posSide?: "long" | "short";
-    ordType!:
-        | "conditional"
-        | "oco"
-        | "trigger"
-        | "move_order_stop"
-        | "iceberg"
-        | "twap";
+  instId!: string;
+  tdMode!: "cash" | "isolated" | "cross";
+  ccy?: string;
+  side!: "buy" | "sell";
+  posSide?: "long" | "short";
+  ordType!:
+    | "conditional"
+    | "oco"
+    | "trigger"
+    | "move_order_stop"
+    | "iceberg"
+    | "twap";
 
-    sz!: string;
-    tag?: string;
-    reduceOnly?: boolean;
-    tgtCcy?: "quote_ccy" | "base_ccy";
-    clOrdId?: string;
+  sz!: string;
+  tag?: string;
+  reduceOnly?: boolean;
+  tgtCcy?: "quote_ccy" | "base_ccy";
+  clOrdId?: string;
 }
 
 export class AlgoStopTpInputDto {
-    tpOrdPx?: string;
-    tpTriggerPxType?: string;
-    tpTriggerPx?: string;
-    slTriggerPx?: string;
-    slTriggerPxType?: "last" | "mark" | "index";
-    slOrdPx?: string;
+  tpOrdPx?: string;
+  tpTriggerPxType?: string;
+  tpTriggerPx?: string;
+  slTriggerPx?: string;
+  slTriggerPxType?: "last" | "mark" | "index";
+  slOrdPx?: string;
 }
 
 export class AlgoTrigetInputDto {
-    triggerPx!: string;
-    orderPx!: string;
-    triggerPxType?: "last" | "mark" | "index";
+  triggerPx!: string;
+  orderPx!: string;
+  triggerPxType?: "last" | "mark" | "index";
 }
 
 export class AlgoTrailStopInputDto {
-    callbackRatio?: string;
-    callbackSpread?: string;
-    activePx?: string;
+  callbackRatio?: string;
+  callbackSpread?: string;
+  activePx?: string;
 }
 
 export class AlgoIcebergInputDto {
-    pxVar?: string;
-    pxSpread?: string;
-    szLimit!: string;
-    pxLimit!: string;
+  pxVar?: string;
+  pxSpread?: string;
+  szLimit!: string;
+  pxLimit!: string;
 }
 
 export class AlgoTWAPInputDto {
-    pxVar?: string;
-    pxSpread?: string;
-    szLimit!: string;
-    pxLimit!: string;
-    timeInterval!: string;
+  pxVar?: string;
+  pxSpread?: string;
+  szLimit!: string;
+  pxLimit!: string;
+  timeInterval!: string;
 }
 
 export class AlgoOrderResponseDto {
-    algoId!: string;
-    clOrdId!: string;
-    sCode!: string;
-    sMsg!: string;
+  algoId!: string;
+  clOrdId!: string;
+  sCode!: string;
+  sMsg!: string;
 }
 
 export class CancelAlgoOrderInputDto {
-    algoId!: string;
-    instId!: string
+  algoId!: string;
+  instId!: string;
 }
 export class CancelAlgoOrderResponseDto {
-    algoId!: string;
-    sCode!: string;
-    sMsg!: string;
+  algoId!: string;
+  sCode!: string;
+  sMsg!: string;
+}
+
+export class AlgoOrderHistoryInput {
+  ordType!:
+    | "conditional"
+    | "oco"
+    | "trigger"
+    | "move_order_stop"
+    | "iceberg"
+    | "twap";
+  state!: "effective" | "canceled" | "order_failed";
+  algoId!: string;
+  instType?: "MARGIN" | "FUTURES" | "SWAP" | "SPOT";
+  instId?: string;
+  after?: string;
+  before?: string;
+  limit?: string;
+}
+
+export class AlgoOrderHistoryResponse {
+  instType!: string;
+  instId!: string;
+  ordId!: string;
+  ccy!: string;
+  clOrdId!: string;
+  algoId!: string;
+  sz!: string;
+  closeFraction!: string;
+  ordType!: string;
+  side!: string;
+  posSide!: string;
+  tdMode!: string;
+  tgtCcy!: string;
+  state!: "effective" | "canceled" | "order_failed";
+  lever!: string;
+  tpTriggerPx!: string;
+  tpTriggerPxType!: "mark" | "index" | "last";
+  tpOrdPx!: string;
+  slTriggerPx!: string;
+  slTriggerPxType!: "mark" | "index" | "last";
+  triggerPx!: string;
+  triggerPxType!: "mark" | "index" | "last";
+  ordPx!: string;
+  actualSz!: string;
+  actualPx!: string;
+  actualSide!: "sl" | "tp";
+  pxVar!: string;
+  pxSpread!: string;
+  pxLimit!: string;
+  szLimit!: string;
+  timeInterval!: string;
+  callbackRatio!: string;
+  callbackSpread!: string;
+  activePx!: string;
+  moveTriggerPx!: string;
+  reduceOnly!: string;
+  triggerTime!: string;
+  last!: string;
+  cTime!: string;
 }
--- src/httpApi.ts
+++ src/httpApi.ts
@@ -5,219 +5,283 @@ import { InstTyp } from "./dto/InstTyp";
 import { BalanceDto } from "./dto/balance.dto";
 import { ConfigDto } from "./dto/config.dto";
 import { OkxResponse } from "./dto/okx-response.dto";
-import { ClosePositionsInputDto, ClosePositionsResponseDto, PositionDto } from "./dto/position.dto";
+import {
+  ClosePositionsInputDto,
+  ClosePositionsResponseDto,
+  PositionDto,
+} from "./dto/position.dto";
 import { LeverageReponseDto, LeverageInputDto } from "./dto/leverage.dto";
-import { OrderInpoutDto, OrderResponseDto, OrderDetails, OrderListInput, AlgoOrderInpoutDto, AlgoOrderResponseDto, AlgoStopTpInputDto, AlgoTrailStopInputDto, AlgoTrigetInputDto, AlgoIcebergInputDto, AlgoTWAPInputDto, CancelAlgoOrderInputDto, AlgoOrderListInput, AlgoOrderDetails } from "./dto/order.dto";
-import { CancelOrderInputDto, CancelOrderResponseDto } from "./dto/cancel-order.dto";
+import {
+  OrderInpoutDto,
+  OrderResponseDto,
+  OrderDetails,
+  OrderListInput,
+  AlgoOrderInpoutDto,
+  AlgoOrderResponseDto,
+  AlgoStopTpInputDto,
+  AlgoTrailStopInputDto,
+  AlgoTrigetInputDto,
+  AlgoIcebergInputDto,
+  AlgoTWAPInputDto,
+  CancelAlgoOrderInputDto,
+  AlgoOrderListInput,
+  AlgoOrderDetails,
+  AlgoOrderHistoryInput,
+  AlgoOrderHistoryResponse,
+} from "./dto/order.dto";
+import {
+  CancelOrderInputDto,
+  CancelOrderResponseDto,
+} from "./dto/cancel-order.dto";
 import { CurrencyDto } from "./dto/currency.dto";
 import { InstrumentDto } from "./dto/instrument.dto";
 class HttpApi {
-    apiClient: AxiosInstance;
-    signer: any;
-    apiKey: string;
-    passphrase: string;
-    apiSecret: string;
-    constructor(
-        apiKey: string,
-        apiSecret: string,
-        passphrase: string,
-        baseURL = ""
-    ) {
-        baseURL = baseURL || "https://www.okx.com/";
-        this.apiSecret = apiSecret;
-        this.apiKey = apiKey;
-        this.passphrase = passphrase;
-        const config: AxiosRequestConfig = {
-            baseURL,
-        };
-        this.apiClient = axios.create(config);
+  apiClient: AxiosInstance;
+  signer: any;
+  apiKey: string;
+  passphrase: string;
+  apiSecret: string;
+  constructor(
+    apiKey: string,
+    apiSecret: string,
+    passphrase: string,
+    baseURL = ""
+  ) {
+    baseURL = baseURL || "https://www.okx.com/";
+    this.apiSecret = apiSecret;
+    this.apiKey = apiKey;
+    this.passphrase = passphrase;
+    const config: AxiosRequestConfig = {
+      baseURL,
+    };
+    this.apiClient = axios.create(config);
+  }
+
+  getSignedHeader(method: string, path: string, params: any = "") {
+    const signData = sign(this.apiSecret, path, params, method);
+    return {
+      "OK-ACCESS-KEY": this.apiKey,
+      "OK-ACCESS-TIMESTAMP": signData[0],
+      "OK-ACCESS-SIGN": signData[1],
+      "OK-ACCESS-PASSPHRASE": this.passphrase,
+    };
+  }
+
+  async get<T>(path: string, params: any = ""): Promise<T> {
+    if (params) {
+      for (const key of Object.keys(params || {})) {
+        if (params[key] === null || params[key] === undefined)
+          delete params[key];
+      }
+      path += "?" + qs.stringify(params);
     }
 
-    getSignedHeader(method: string, path: string, params: any = "") {
-        const signData = sign(this.apiSecret, path, params, method);
-        return {
-            "OK-ACCESS-KEY": this.apiKey,
-            "OK-ACCESS-TIMESTAMP": signData[0],
-            "OK-ACCESS-SIGN": signData[1],
-            "OK-ACCESS-PASSPHRASE": this.passphrase,
-        };
-    }
+    const res = await this.apiClient.get<any, AxiosResponse<T, any>, T>(path, {
+      headers: this.getSignedHeader("GET", path),
+    });
+    return res.data;
+  }
 
-    async get<T>(path: string, params: any = ""): Promise<T> {
-        if (params) {
-            for (const key of Object.keys(params || {})) {
-                if (params[key] === null || params[key] === undefined)
-                    delete params[key];
-            }
-            path += "?" + qs.stringify(params);
-        }
+  async post<T>(path: string, body: any): Promise<T> {
+    const headers = {
+      ...this.getSignedHeader("POST", path, body),
+      "Content-Type": "application/json",
+    };
 
-        const res = await this.apiClient.get<any, AxiosResponse<T, any>, T>(path, {
-            headers: this.getSignedHeader("GET", path),
-        });
-        return res.data;
-    }
+    const res = await this.apiClient.post<any, AxiosResponse<T, any>, T>(
+      path,
+      body,
+      { headers }
+    );
+    return res.data;
+  }
 
-    async post<T>(path: string, body: any): Promise<T> {
-        const headers = {
-            ...this.getSignedHeader("POST", path, body),
-            "Content-Type": "application/json",
-        };
+  async getTickers(instType: InstTyp) {
+    const {
+      data: { data },
+    } = await this.apiClient.get("/api/v5/market/tickers", {
+      params: { instType },
+    });
+    return data;
+  }
 
-        const res = await this.apiClient.post<any, AxiosResponse<T, any>, T>(
-            path,
-            body,
-            { headers }
-        );
-        return res.data;
-    }
+  async getAccountConfig(): Promise<ConfigDto> {
+    const res = await this.get<OkxResponse<ConfigDto[]>>(
+      "/api/v5/account/config"
+    );
+    return res.data[0];
+  }
 
-    async getTickers(instType: InstTyp) {
-        const {
-            data: { data },
-        } = await this.apiClient.get("/api/v5/market/tickers", {
-            params: { instType },
-        });
-        return data;
-    }
+  async getBalance(coins: string = ""): Promise<BalanceDto> {
+    const res = await this.get<OkxResponse<BalanceDto[]>>(
+      "/api/v5/account/balance",
+      { ccy: coins }
+    );
+    return res.data[0];
+  }
 
-    async getAccountConfig(): Promise<ConfigDto> {
-        const res = await this.get<OkxResponse<ConfigDto[]>>(
-            "/api/v5/account/config"
-        );
-        return res.data[0];
-    }
+  async getPositions(
+    instType: InstTyp | string = "",
+    instId: string = "",
+    posId: string = ""
+  ): Promise<PositionDto[]> {
+    const res = await this.get<OkxResponse<PositionDto[]>>(
+      "/api/v5/account/positions",
+      { instType, instId, posId }
+    );
+    return res.data;
+  }
 
-    async getBalance(coins: string = ""): Promise<BalanceDto> {
-        const res = await this.get<OkxResponse<BalanceDto[]>>(
-            "/api/v5/account/balance",
-            { ccy: coins }
-        );
-        return res.data[0];
-    }
+  async setLeverage(input: LeverageInputDto): Promise<LeverageReponseDto> {
+    const res = await this.post<OkxResponse<LeverageReponseDto[]>>(
+      "/api/v5/account/set-leverage",
+      input
+    );
+    return res.data[0];
+  }
 
-    async getPositions(
-        instType: InstTyp | string = "",
-        instId: string = "",
-        posId: string = ""
-    ): Promise<PositionDto[]> {
-        const res = await this.get<OkxResponse<PositionDto[]>>(
-            "/api/v5/account/positions",
-            { instType, instId, posId }
-        );
-        return res.data;
-    }
+  async placeOrder(input: OrderInpoutDto): Promise<OrderResponseDto> {
+    const res = await this.post<OkxResponse<OrderResponseDto[]>>(
+      "/api/v5/trade/order",
+      input
+    );
+    return res.data[0];
+  }
 
-    async setLeverage(input: LeverageInputDto): Promise<LeverageReponseDto> {
-        const res = await this.post<OkxResponse<LeverageReponseDto[]>>(
-            "/api/v5/account/set-leverage",
-            input
-        );
-        return res.data[0];
-    }
+  async placeMultipleOrders(
+    input: OrderInpoutDto[]
+  ): Promise<OrderResponseDto[]> {
+    const res = await this.post<OkxResponse<OrderResponseDto[]>>(
+      "/api/v5/trade/batch-orders",
+      input
+    );
+    return res.data;
+  }
 
-    async placeOrder(input: OrderInpoutDto): Promise<OrderResponseDto> {
-        const res = await this.post<OkxResponse<OrderResponseDto[]>>(
-            "/api/v5/trade/order",
-            input
-        );
-        return res.data[0];
-    }
+  async cancelOrder(
+    input: CancelOrderInputDto
+  ): Promise<CancelOrderResponseDto> {
+    const res = await this.post<OkxResponse<CancelOrderResponseDto[]>>(
+      "/api/v5/trade/cancel-order",
+      input
+    );
+    return res.data[0];
+  }
 
-    async placeMultipleOrders(input: OrderInpoutDto[]): Promise<OrderResponseDto[]> {
-        const res = await this.post<OkxResponse<OrderResponseDto[]>>(
-            "/api/v5/trade/batch-orders",
-            input
-        );
-        return res.data;
-    }
+  async cancelMultipleOrders(
+    input: CancelOrderInputDto[]
+  ): Promise<CancelOrderResponseDto[]> {
+    const res = await this.post<OkxResponse<CancelOrderResponseDto[]>>(
+      "/api/v5/trade/cancel-batch-orders",
+      input
+    );
+    return res.data;
+  }
 
-    async cancelOrder(input: CancelOrderInputDto): Promise<CancelOrderResponseDto> {
-        const res = await this.post<OkxResponse<CancelOrderResponseDto[]>>(
-            "/api/v5/trade/cancel-order",
-            input
-        );
-        return res.data[0];
-    }
+  async getOrderDetails(
+    instId: string,
+    ordId: string = "",
+    clOrdId: string = ""
+  ): Promise<OrderDetails> {
+    const res = await this.get<OkxResponse<OrderDetails[]>>(
+      "/api/v5/trade/order",
+      { instId, ordId, clOrdId }
+    );
+    return res.data[0];
+  }
 
-    async cancelMultipleOrders(input: CancelOrderInputDto[]): Promise<CancelOrderResponseDto[]> {
-        const res = await this.post<OkxResponse<CancelOrderResponseDto[]>>(
-            "/api/v5/trade/cancel-batch-orders",
-            input
-        );
-        return res.data;
-    }
+  async getOrderList(
+    input: OrderListInput | undefined = undefined
+  ): Promise<OrderDetails[]> {
+    const res = await this.get<OkxResponse<OrderDetails[]>>(
+      "/api/v5/trade/orders-pending",
+      input
+    );
+    return res.data;
+  }
 
-    async getOrderDetails(instId: string, ordId: string = "", clOrdId: string = ""): Promise<OrderDetails> {
-        const res = await this.get<OkxResponse<OrderDetails[]>>(
-            "/api/v5/trade/order",
-            { instId, ordId, clOrdId }
-        );
-        return res.data[0];
-    }
+  async getAlgoOrderList(
+    input: AlgoOrderListInput | undefined = undefined
+  ): Promise<AlgoOrderDetails[]> {
+    const res = await this.get<OkxResponse<AlgoOrderDetails[]>>(
+      "/api/v5/trade/orders-algo-pending",
+      input
+    );
+    return res.data;
+  }
 
-    async getOrderList(input: OrderListInput | undefined = undefined): Promise<OrderDetails[]> {
-        const res = await this.get<OkxResponse<OrderDetails[]>>(
-            "/api/v5/trade/orders-pending",
-            input
-        );
-        return res.data;
-    }
+  async getAlgoOrderHistory(
+    input: AlgoOrderHistoryInput | undefined = undefined
+  ): Promise<AlgoOrderHistoryResponse[]> {
+    const res = await this.get<OkxResponse<AlgoOrderHistoryResponse[]>>(
+      "/api/v5/trade/orders-algo-history",
+      input
+    );
+    return res.data;
+  }
 
-    async getAlgoOrderList(input: AlgoOrderListInput | undefined = undefined): Promise<AlgoOrderDetails[]> {
-        const res = await this.get<OkxResponse<AlgoOrderDetails[]>>(
-            "/api/v5/trade/orders-algo-pending",
-            input
-        );
-        return res.data;
-    }
+  async closePositions(
+    input: ClosePositionsInputDto
+  ): Promise<ClosePositionsResponseDto[]> {
+    const res = await this.post<OkxResponse<ClosePositionsResponseDto[]>>(
+      "/api/v5/trade/close-position",
+      input
+    );
+    return res.data;
+  }
 
-    async closePositions(input: ClosePositionsInputDto): Promise<ClosePositionsResponseDto[]> {
-        const res = await this.post<OkxResponse<ClosePositionsResponseDto[]>>(
-            "/api/v5/trade/close-position",
-            input
-        );
-        return res.data;
-    }
+  async placeAlgoOrder(
+    input: AlgoOrderInpoutDto,
+    input2:
+      | AlgoStopTpInputDto
+      | AlgoTrailStopInputDto
+      | AlgoTrigetInputDto
+      | AlgoIcebergInputDto
+      | AlgoTWAPInputDto
+  ): Promise<AlgoOrderResponseDto> {
+    const data = { ...input, ...input2 };
+    const res = await this.post<OkxResponse<AlgoOrderResponseDto[]>>(
+      "/api/v5/trade/order-algo",
+      data
+    );
+    return res.data[0];
+  }
 
-    async placeAlgoOrder(input: AlgoOrderInpoutDto, input2: AlgoStopTpInputDto | AlgoTrailStopInputDto | AlgoTrigetInputDto | AlgoIcebergInputDto | AlgoTWAPInputDto): Promise<AlgoOrderResponseDto> {
-        const data = { ...input, ...input2 };
-        const res = await this.post<OkxResponse<AlgoOrderResponseDto[]>>(
-            "/api/v5/trade/order-algo",
-            data
-        );
-        return res.data[0];
-    }
+  async cancelAlgoOrders(
+    input: CancelAlgoOrderInputDto[]
+  ): Promise<AlgoOrderResponseDto> {
+    const res = await this.post<OkxResponse<AlgoOrderResponseDto[]>>(
+      "/api/v5/trade/cancel-algos",
+      input
+    );
+    return res.data[0];
+  }
 
-    async cancelAlgoOrders(input: CancelAlgoOrderInputDto[]): Promise<AlgoOrderResponseDto> {
-        const res = await this.post<OkxResponse<AlgoOrderResponseDto[]>>(
-            "/api/v5/trade/cancel-algos",
-            input
-        );
-        return res.data[0];
-    }
+  async getCurrencies(ccy: string = ""): Promise<CurrencyDto[]> {
+    const res = await this.get<OkxResponse<CurrencyDto[]>>(
+      "/api/v5/asset/currencies",
+      { ccy }
+    );
 
-    async getCurrencies(ccy: string = ""): Promise<CurrencyDto[]> {
-        const res = await this.get<OkxResponse<CurrencyDto[]>>(
-            "/api/v5/asset/currencies",
-            { ccy }
-        );
+    return res.data;
+  }
 
-        return res.data;
-    }
+  async getInstruments(
+    instType: "SPOT" | "MARGIN" | "SWAP" | "FUTURES" | "OPTION",
+    uly: string = "",
+    instId: string = ""
+  ): Promise<InstrumentDto[]> {
+    const res = await this.get<OkxResponse<InstrumentDto[]>>(
+      "/api/v5/public/instruments",
+      {
+        instType,
+        uly,
+        instId,
+      }
+    );
 
-    async getInstruments(instType: "SPOT" | "MARGIN" | "SWAP" | "FUTURES" | "OPTION", uly: string = "", instId: string = ""): Promise<InstrumentDto[]> {
-        const res = await this.get<OkxResponse<InstrumentDto[]>>(
-            "/api/v5/public/instruments",
-            {
-                instType,
-                uly,
-                instId
-            }
-        );
-
-        return res.data;
-    }
+    return res.data;
+  }
 }
 
 export default HttpApi;
